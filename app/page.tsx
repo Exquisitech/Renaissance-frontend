79d1b447d0efee1e5798249b3fbaccd61888f656
Dev KILLJOY <didiauche@gmail.com>
2026-01-21 22:34:16 +0100

    Created Landing page base structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import { Button } from "@/components/ui/button";
+import HomeClient from "./home-client";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col gap-6 items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold">⚽ Renaissance</h1>
-      <Button variant="default">Join the Revolution</Button>
-    </main>
-  );
+    return <HomeClient />;
 }
